3abdc501ce67a452e6a8fb6f773e4b47d1372f5a
Erorr <109611369+Sal0-UA@users.noreply.github.com>
2024-02-16 21:18:42 +0200

    Events page changes
    - Added same pop-ups for events.
    - Events now load from JSON file

---
+++ events.js
@@ -0,0 +1,114 @@
+fetch('./data/news.json')
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json)
+        const overlay = document.getElementById("overlay")
+        const newsList = document.getElementById("events-container")
+        const body = document.getElementById("doc-body")
+
+        json.forEach(item => {
+            let image = document.createElement("img")
+            image.src = item.image
+
+            let title = document.createElement("h2")
+            title.innerHTML = item.title
+
+            let description = document.createElement("h3")
+            description.innerHTML = item.description
+
+            let event = document.createElement("div")
+            event.classList.add("event")
+            event.setAttribute("data-modal-target", "#"+item.id)
+
+            event.appendChild(image)
+            event.appendChild(title)
+            event.appendChild(description)
+
+            newsList.appendChild(event)
+
+            //modals
+
+            let modalTitle = document.createElement("div")
+            modalTitle.classList.add("title")
+            modalTitle.innerText = "Інформація"
+
+            let modalCloseButton = document.createElement("button")
+            modalCloseButton.setAttribute("data-close-button", null)
+            modalCloseButton.classList.add("close-button")
+            modalCloseButton.innerHTML = "×"
+
+            let modalHeader = document.createElement("div")
+            modalHeader.classList.add("modal-header")
+            modalHeader.appendChild(modalTitle)
+            modalHeader.appendChild(modalCloseButton)
+
+
+            let modalPhoto = document.createElement("img")
+            modalPhoto.classList.add("modal-event-img")
+            modalPhoto.src = item.image
+
+            let modalShotText = document.createElement("div")
+            modalShotText.classList.add("modal-event-title")
+            modalShotText.innerHTML = item.title
+
+            let modalMainText = document.createElement("div")
+            modalMainText.classList.add("modal-event-description")
+            modalMainText.innerHTML = item.description
+
+            let modalText = document.createElement("div")
+            modalText.classList.add("text")
+            modalText.appendChild(modalShotText)
+            modalText.appendChild(modalMainText)
+
+            let modalBody = document.createElement("div")
+            modalBody.classList.add("modal-body")
+            modalBody.appendChild(modalPhoto)
+            modalBody.appendChild(modalText)
+
+            let modal = document.createElement("div")
+            modal.setAttribute("id", item.id)
+            modal.classList.add("modal")
+            modal.appendChild(modalHeader)
+            modal.appendChild(modalBody)
+
+            body.appendChild(modal)
+        })
+
+        const openModalButtons = document.querySelectorAll('[data-modal-target]')
+        const closeModalButtons = document.querySelectorAll('[data-close-button]')
+        console.log(openModalButtons, closeModalButtons);
+
+        openModalButtons.forEach(button => {
+            button.addEventListener('click', () => {
+              const modal = document.querySelector(button.dataset.modalTarget)
+              openModal(modal)
+            })
+        })
+
+        overlay.addEventListener('click', () => {
+            const modals = document.querySelectorAll('.modal.active')
+            modals.forEach(modal => {
+              closeModal(modal)
+            })
+        })
+
+        closeModalButtons.forEach(button => {
+          button.addEventListener('click', () => {
+            const modal = button.closest('.modal')
+            closeModal(modal)
+            })
+        })
+
+        function openModal(modal) {
+            if (modal == null) return
+            modal.classList.add('active')
+            overlay.classList.add('active')
+        }
+      
+          function closeModal(modal) {
+            if (modal == null) return
+            modal.classList.remove('active')
+            overlay.classList.remove('active')
+        }
+
+    })
