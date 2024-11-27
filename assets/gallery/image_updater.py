436fd997b932b36ad45b832e5067c5ecaeec56f5
Erorr <maxymholub@gmail.com>
2024-11-27 23:21:49 +0200

    New event, new members, fixes

--- assets/gallery/image_updater.py
+++ assets/gallery/image_updater.py
@@ -1,13 +1,18 @@
 import os
 import json
 
-# images size: 1280x960
-
+# Images size: 1280x960
 allowed_types = [".jpg", ".png", ".webp", ".jpeg"]
-files = []
+image_metadata = []
 i = 1
 my_path = os.getcwd() + "\\assets\\gallery"
 print(f"my_path: {my_path}")
+
+# Get all files in the directory with their full paths
+files = [os.path.join(my_path, f) for f in os.listdir(my_path) if os.path.isfile(os.path.join(my_path, f))]
+
+# Sort files by modification time
+files.sort(key=lambda x: os.path.getmtime(x))
 
 
 # Function to check if a file object has an allowed file type
@@ -16,19 +21,20 @@ def is_allowed_file(filepath):
     return ext.lower() in allowed_types  # Check if in allowed_types
 
 
-for file in os.listdir(my_path):
-    if os.path.isfile(os.path.join(my_path, file)):
-        if is_allowed_file(file):
-            print(f"file:{file}")
-            files.append({
-                "id": i,
-                "path": "./assets/gallery/"+file})
-            i += 1
+for file in files:
+    if is_allowed_file(file):
+        print(f"file: {file} \n")
+        image_metadata.append({
+            "id": i,
+            "path": "./assets/gallery/" + os.path.basename(file)  # Use only the filename
+        })
+        i += 1
 
-files.reverse()
-print(files)
+image_metadata.reverse()
+print(image_metadata)
 
-with open('assets\\gallery\\images.json', 'w') as file:
-    json.dump(files, file)
+# Save metadata to JSON
+with open('assets\\gallery\\images.json', 'w') as json_file:
+    json.dump(image_metadata, json_file, indent=4)
 
 print("Done!")
